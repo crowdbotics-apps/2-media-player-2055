94b197d0a52b94f01a419e6ec9b65037e92a0b28
Crowdbotics <team@crowdbotics.com>
2019-04-07 19:37:21 +0000

    initial commit

---
+++ android/app/src/main/java/com/2_media_player_2055/MainActivity.java
@@ -0,0 +1,15 @@
+package com.2_media_player_2055;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "2_media_player_2055";
+    }
+}
